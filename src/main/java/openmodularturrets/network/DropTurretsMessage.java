4217cb701032220a6971b45d5c4e661bc2c665f8
Poenjabiesous <nielverster@gmail.com>
2014-11-24 21:30:38 +0200

    Drop Turrets and Drop Base gui buttons work now
    Signed-off-by: Poenjabiesous <nielverster@gmail.com>

---
+++ src/main/java/openmodularturrets/network/DropTurretsMessage.java
@@ -0,0 +1,98 @@
+package openmodularturrets.network;
+
+import cpw.mods.fml.common.network.ByteBufUtils;
+import cpw.mods.fml.common.network.simpleimpl.IMessage;
+import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
+import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import io.netty.buffer.ByteBuf;
+import openmodularturrets.tileentity.turretBase.TurretBase;
+import openmodularturrets.tileentity.turrets.TurretHead;
+import net.minecraft.world.World;
+
+public class DropTurretsMessage implements IMessage,
+		IMessageHandler<DropTurretsMessage, IMessage> {
+	private int x, y, z;
+	private String player;
+
+	public DropTurretsMessage() {
+	}
+
+	public DropTurretsMessage(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	@Override
+	public void fromBytes(ByteBuf buf) {
+		this.x = buf.readInt();
+		this.y = buf.readInt();
+		this.z = buf.readInt();
+	}
+
+	@Override
+	public void toBytes(ByteBuf buf) {
+		buf.writeInt(this.x);
+		buf.writeInt(this.y);
+		buf.writeInt(this.z);
+	}
+
+	@Override
+	public IMessage onMessage(DropTurretsMessage message, MessageContext ctx) {
+		World world = ctx.getServerHandler().playerEntity.worldObj;
+
+		if (world.getTileEntity(message.getX() + 1, message.getY(),
+				message.getZ()) instanceof TurretHead) {
+			world.func_147480_a(message.getX() + 1, message.getY(),
+					message.getZ(), true);
+		}
+
+		if (world.getTileEntity(message.getX() - 1, message.getY(),
+				message.getZ()) instanceof TurretHead) {
+			world.func_147480_a(message.getX() - 1, message.getY(),
+					message.getZ(), true);
+		}
+
+		if (world.getTileEntity(message.getX(), message.getY() + 1,
+				message.getZ()) instanceof TurretHead) {
+			world.func_147480_a(message.getX(), message.getY() + 1,
+					message.getZ(), true);
+		}
+
+		if (world.getTileEntity(message.getX(), message.getY() - 1,
+				message.getZ()) instanceof TurretHead) {
+			world.func_147480_a(message.getX(), message.getY() - 1,
+					message.getZ(), true);
+		}
+
+		if (world.getTileEntity(message.getX(), message.getY(),
+				message.getZ() + 1) instanceof TurretHead) {
+			world.func_147480_a(message.getX(), message.getY(),
+					message.getZ() + 1, true);
+		}
+
+		if (world.getTileEntity(message.getX(), message.getY(),
+				message.getZ() - 1) instanceof TurretHead) {
+			world.func_147480_a(message.getX(), message.getY(),
+					message.getZ() - 1, true);
+		}
+
+		return null;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public String getOwner() {
+		return player;
+	}
+}
