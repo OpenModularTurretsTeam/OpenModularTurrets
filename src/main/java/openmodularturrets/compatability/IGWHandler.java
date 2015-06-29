d46d25b6d713d2a02d5a702a4b8a6796191b1871
niel.verster <niel.verster@rorotika.com>
2015-06-29 20:16:42 +0200

    Pre-release commit. Don't use this build, this is part of the last pre-flight checks
    Signed-off-by: niel.verster <niel.verster@rorotika.com>

--- src/main/java/openmodularturrets/compatability/IGWHandler.java
+++ src/main/java/openmodularturrets/compatability/IGWHandler.java
@@ -2,6 +2,7 @@ package openmodularturrets.compatability;
 
 import igwmod.api.WikiRegistry;
 import openmodularturrets.blocks.Blocks;
+import openmodularturrets.handler.ConfigHandler;
 
 /**
  * Created by Keridos on 23/01/2015.
@@ -26,16 +27,53 @@ public class IGWHandler {
 
         // this is a workaround until Minemaarten enables putting the wiki stuff in our own asset folder, for now its
         // in a subfolder in the igwmod assets
-        WikiRegistry.registerBlockAndItemPageEntry(Blocks.disposableItemTurret,
-                                                   "openmodularturrets/block/disposableItemTurret");
         WikiRegistry.registerBlockAndItemPageEntry(Blocks.leverBlock, "openmodularturrets/block/leverBlock");
-        WikiRegistry.registerBlockAndItemPageEntry(Blocks.grenadeLauncherTurret,
-                                                   "openmodularturrets/block/grenadeLauncherTurret");
-        WikiRegistry.registerBlockAndItemPageEntry(Blocks.laserTurret, "openmodularturrets/block/laserTurret");
-        WikiRegistry.registerBlockAndItemPageEntry(Blocks.machineGunTurret,
-                                                   "openmodularturrets/block/machineGunTurret");
-        WikiRegistry.registerBlockAndItemPageEntry(Blocks.rocketTurret, "openmodularturrets/block/rocketTurret");
-        WikiRegistry.registerBlockAndItemPageEntry(Blocks.railGunTurret, "openmodularturrets/block/railGunTurret");
+
+        if (ConfigHandler.getDisposableTurretSettings().isEnabled()) {
+            WikiRegistry.registerBlockAndItemPageEntry(Blocks.disposableItemTurret,
+                    "openmodularturrets/block/disposableItemTurret");
+        }
+
+        if (ConfigHandler.getPotatoCannonTurretSettings().isEnabled()) {
+            WikiRegistry.registerBlockAndItemPageEntry(Blocks.potatoCannonTurret,
+                    "openmodularturrets/block/potatoCannonTurret");
+        }
+
+        if (ConfigHandler.getMachineGunTurretSettings().isEnabled()) {
+            WikiRegistry.registerBlockAndItemPageEntry(Blocks.machineGunTurret,
+                    "openmodularturrets/block/machineGunTurret");
+        }
+
+        if (ConfigHandler.getIncendiary_turret().isEnabled()) {
+            WikiRegistry.registerBlockAndItemPageEntry(Blocks.incendiaryTurret,
+                    "openmodularturrets/block/incendiaryTurret");
+        }
+
+        if (ConfigHandler.getGrenadeTurretSettings().isEnabled()) {
+            WikiRegistry.registerBlockAndItemPageEntry(Blocks.grenadeLauncherTurret,
+                    "openmodularturrets/block/grenadeLauncherTurret");
+        }
+
+        if (ConfigHandler.getRelativistic_turret().isEnabled()) {
+            WikiRegistry.registerBlockAndItemPageEntry(Blocks.relativisticTurret,
+                    "openmodularturrets/block/relativisticTurret");
+        }
+        if (ConfigHandler.getRocketTurretSettings().isEnabled()) {
+            WikiRegistry.registerBlockAndItemPageEntry(Blocks.rocketTurret, "openmodularturrets/block/rocketTurret");
+        }
+
+        if (ConfigHandler.getTeleporter_turret().isEnabled()) {
+            WikiRegistry.registerBlockAndItemPageEntry(Blocks.teleporterTurret, "openmodularturrets/block/teleporterTurret");
+        }
+
+        if (ConfigHandler.getLaserTurretSettings().isEnabled()) {
+            WikiRegistry.registerBlockAndItemPageEntry(Blocks.laserTurret, "openmodularturrets/block/laserTurret");
+        }
+
+        if (ConfigHandler.getRailgun_turret().isEnabled()) {
+            WikiRegistry.registerBlockAndItemPageEntry(Blocks.railGunTurret, "openmodularturrets/block/railGunTurret");
+        }
+
         WikiRegistry.registerBlockAndItemPageEntry(Blocks.turretBaseTierOne, "openmodularturrets/block/turretBase");
         WikiRegistry.registerBlockAndItemPageEntry(Blocks.turretBaseTierTwo, "openmodularturrets/block/turretBase");
         WikiRegistry.registerBlockAndItemPageEntry(Blocks.turretBaseTierThree, "openmodularturrets/block/turretBase");
