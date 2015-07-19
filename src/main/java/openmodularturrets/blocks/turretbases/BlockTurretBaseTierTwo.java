298389be1d6680d08bc5e0a17e38e9eb1b08017a
Poenjabiesous <nielverster@gmail.com>
2015-07-19 23:16:58 +0200

    Turret bases now camo-able with any standard non opaque cube-block
    Signed-off-by: Poenjabiesous <nielverster@gmail.com>

--- src/main/java/openmodularturrets/blocks/turretbases/BlockTurretBaseTierTwo.java
+++ src/main/java/openmodularturrets/blocks/turretbases/BlockTurretBaseTierTwo.java
@@ -2,6 +2,8 @@ package openmodularturrets.blocks.turretbases;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import openmodularturrets.handler.ConfigHandler;
 import openmodularturrets.reference.ModInfo;
@@ -25,6 +27,15 @@ public class BlockTurretBaseTierTwo extends BlockAbstractTurretBase {
         super.registerBlockIcons(p_149651_1_);
 
         blockIcon = p_149651_1_.registerIcon(ModInfo.ID.toLowerCase() + ":turretBaseTierTwo");
+    }
+
+    @Override
+    public IIcon getIcon(IBlockAccess p_149673_1_, int p_149673_2_, int p_149673_3_, int p_149673_4_, int p_149673_5_) {
+        if (this.camoStack == null) {
+            return blockIcon;
+        } else {
+            return this.camoStack.getIconIndex();
+        }
     }
 
     @Override
