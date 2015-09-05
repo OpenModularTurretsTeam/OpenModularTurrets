9b272a477f9bcd6885c7bc12146a9e1889ba3351
Poenjabiesous <nielverster@gmail.com>
2015-09-05 08:34:05 +0200

    Fixes #163, #158, #155 and #137
    Signed-off-by: Poenjabiesous <nielverster@gmail.com>

--- src/main/java/openmodularturrets/blocks/misc/BlockFenceTierOne.java
+++ src/main/java/openmodularturrets/blocks/misc/BlockFenceTierOne.java
@@ -4,9 +4,13 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockPane;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.Item;
 import openmodularturrets.ModularTurrets;
+import openmodularturrets.blocks.Blocks;
 import openmodularturrets.reference.ModInfo;
 import openmodularturrets.reference.Names;
+
+import java.util.Random;
 
 public class BlockFenceTierOne extends BlockPane {
     public BlockFenceTierOne() {
@@ -14,7 +18,7 @@ public class BlockFenceTierOne extends BlockPane {
         this.setCreativeTab(ModularTurrets.modularTurretsTab);
         this.setResistance(10.0F);
         this.setHardness(10.0F);
-        this.setHarvestLevel("pickaxe", 3);
+        this.setHarvestLevel("pickaxe", 2);
         this.setStepSound(Block.soundTypeStone);
         this.setBlockName(Names.Blocks.unlocalisedFenceTierOne);
     }
@@ -23,5 +27,10 @@ public class BlockFenceTierOne extends BlockPane {
     public void registerBlockIcons(IIconRegister p_149651_1_) {
         super.registerBlockIcons(p_149651_1_);
         blockIcon = p_149651_1_.registerIcon(ModInfo.ID.toLowerCase() + ":fenceTierOne");
+    }
+
+    @Override
+    public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_) {
+        return Item.getItemFromBlock(Blocks.fenceTierOne);
     }
 }
--- src/main/java/openmodularturrets/blocks/misc/BlockFenceTierTwo.java
+++ src/main/java/openmodularturrets/blocks/misc/BlockFenceTierTwo.java
@@ -5,11 +5,16 @@ import net.minecraft.block.BlockPane;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.Item;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 import openmodularturrets.ModularTurrets;
+import openmodularturrets.blocks.Blocks;
 import openmodularturrets.reference.ModInfo;
 import openmodularturrets.reference.Names;
+
+import java.util.Random;
 
 public class BlockFenceTierTwo extends BlockPane {
     public BlockFenceTierTwo() {
@@ -17,7 +22,7 @@ public class BlockFenceTierTwo extends BlockPane {
         this.setCreativeTab(ModularTurrets.modularTurretsTab);
         this.setResistance(20.0F);
         this.setHardness(20.0F);
-        this.setHarvestLevel("pickaxe", 3);
+        this.setHarvestLevel("pickaxe", 2);
         this.setStepSound(Block.soundTypeStone);
         this.setBlockName(Names.Blocks.unlocalisedFenceTierTwo);
     }
@@ -30,6 +35,11 @@ public class BlockFenceTierTwo extends BlockPane {
 
     @Override
     public void onEntityCollidedWithBlock(World p_149670_1_, int p_149670_2_, int p_149670_3_, int p_149670_4_, Entity p_149670_5_) {
-        p_149670_5_.attackEntityFrom(DamageSource.cactus, 2);
+        if(!(p_149670_5_ instanceof EntityItem))  p_149670_5_.attackEntityFrom(DamageSource.cactus, 2);
+    }
+
+    @Override
+    public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_) {
+        return Item.getItemFromBlock(Blocks.fenceTierTwo);
     }
 }
