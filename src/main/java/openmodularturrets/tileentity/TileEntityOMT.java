37a230ecfb850b0a7e56e3a380602a16a2522045
Keridos <fuffzehn@ironhide.de>
2015-12-09 03:55:01 +0100

    some more refactoring

--- src/main/java/openmodularturrets/tileentity/TileEntityOMT.java
+++ src/main/java/openmodularturrets/tileentity/TileEntityOMT.java
@@ -1,6 +1,8 @@
 package openmodularturrets.tileentity;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 
 /**
@@ -17,5 +19,12 @@ public class TileEntityOMT extends TileEntity {
     @Override
     public void readFromNBT(NBTTagCompound par1) {
         super.readFromNBT(par1);
+    }
+
+    @Override
+    public Packet getDescriptionPacket() {
+        NBTTagCompound var1 = new NBTTagCompound();
+        this.writeToNBT(var1);
+        return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 2, var1);
     }
 }
