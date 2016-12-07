98d2e058bdb5f8f7deaca1e49676d8298f98bc1f
Keridos <2742845+Keridos@users.noreply.github.com>
2016-12-07 23:07:28 +0100

    fixed base recipes, change item, block and sound registration to use reflection, fixed recycler addon localization and update omlib dependency

--- src/main/java/omtteam/openmodularturrets/blocks/turretheads/BlockRelativisticTurret.java
+++ src/main/java/omtteam/openmodularturrets/blocks/turretheads/BlockRelativisticTurret.java
@@ -1,10 +1,13 @@
 package omtteam.openmodularturrets.blocks.turretheads;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import omtteam.openmodularturrets.items.blocks.ItemBlockRelativisticTurret;
 import omtteam.openmodularturrets.reference.Names;
 import omtteam.openmodularturrets.reference.Reference;
 import omtteam.openmodularturrets.tileentity.turrets.RelativisticTurretTileEntity;
@@ -16,6 +19,11 @@ public class BlockRelativisticTurret extends BlockAbstractTurretHead {
         super();
         this.setUnlocalizedName(Names.Blocks.relativisticTurret);
         this.setRegistryName(Reference.MOD_ID, Names.Blocks.relativisticTurret);
+    }
+
+    @Override
+    public ItemBlock getItemBlock(Block block) {
+        return new ItemBlockRelativisticTurret(block);
     }
 
     @Override
--- src/main/java/omtteam/openmodularturrets/init/ModBlocks.java
+++ src/main/java/omtteam/openmodularturrets/init/ModBlocks.java
@@ -7,12 +7,14 @@ import omtteam.openmodularturrets.blocks.BlockTurretBase;
 import omtteam.openmodularturrets.blocks.LeverBlock;
 import omtteam.openmodularturrets.blocks.turretheads.*;
 import omtteam.openmodularturrets.handler.ConfigHandler;
-import omtteam.openmodularturrets.items.blocks.*;
 import omtteam.openmodularturrets.reference.Names;
 import omtteam.openmodularturrets.tileentity.Expander;
 import omtteam.openmodularturrets.tileentity.LeverTileEntity;
 import omtteam.openmodularturrets.tileentity.TurretBase;
 import omtteam.openmodularturrets.tileentity.turrets.*;
+
+import static omtteam.omlib.util.ReflectionInitHelper.registerBlocks;
+
 
 public class ModBlocks {
     public static Block turretBase;
@@ -38,77 +40,53 @@ public class ModBlocks {
 
     public static void initBlocks() {
         turretBase = new BlockTurretBase();
-        GameRegistry.register(turretBase);
-        GameRegistry.register(new ItemBlockTurretBase(turretBase));
-
         expander = new BlockExpander();
-        GameRegistry.register(expander);
-        GameRegistry.register(new ItemBlockExpander(expander));
 
         if (ConfigHandler.getDisposableTurretSettings().isEnabled()) {
             disposableItemTurret = new BlockDisposableTurret();
-            GameRegistry.register(disposableItemTurret);
-            GameRegistry.register(new ItemBlockDisposableTurret(disposableItemTurret));
         }
 
         if (ConfigHandler.getPotatoCannonTurretSettings().isEnabled()) {
             potatoCannonTurret = new BlockPotatoCannonTurret();
-            GameRegistry.register(potatoCannonTurret);
-            GameRegistry.register(new ItemBlockPotatoCannonTurret(potatoCannonTurret));
         }
 
         if (ConfigHandler.getGunTurretSettings().isEnabled()) {
             machineGunTurret = new BlockGunTurret();
-            GameRegistry.register(machineGunTurret);
-            GameRegistry.register(new ItemBlockMachineGunTurret(machineGunTurret));
         }
 
         if (ConfigHandler.getIncendiary_turret().isEnabled()) {
             incendiaryTurret = new BlockIncendiaryTurret();
-            GameRegistry.register(incendiaryTurret);
-            GameRegistry.register(new ItemBlockIncendiaryTurret(incendiaryTurret));
         }
 
         if (ConfigHandler.getGrenadeTurretSettings().isEnabled()) {
             grenadeLauncherTurret = new BlockGrenadeTurret();
-            GameRegistry.register(grenadeLauncherTurret);
-            GameRegistry.register(new ItemBlockGrenadeLauncherTurret(grenadeLauncherTurret));
         }
 
         if (ConfigHandler.getRelativistic_turret().isEnabled()) {
             relativisticTurret = new BlockRelativisticTurret();
-            GameRegistry.register(relativisticTurret);
-            GameRegistry.register(new ItemBlockRelativisticTurret(relativisticTurret));
         }
 
         if (ConfigHandler.getRocketTurretSettings().isEnabled()) {
             rocketTurret = new BlockRocketTurret();
-            GameRegistry.register(rocketTurret);
-            GameRegistry.register(new ItemBlockRocketTurret(rocketTurret));
         }
 
         if (ConfigHandler.getTeleporter_turret().isEnabled()) {
             teleporterTurret = new BlockTeleporterTurret();
-            GameRegistry.register(teleporterTurret);
-            GameRegistry.register(new ItemBlockTeleporterTurret(teleporterTurret));
         }
 
         if (ConfigHandler.getLaserTurretSettings().isEnabled()) {
             laserTurret = new BlockLaserTurret();
-            GameRegistry.register(laserTurret);
-            GameRegistry.register(new ItemBlockLaserTurret(laserTurret));
         }
 
         if (ConfigHandler.getRailgun_turret().isEnabled()) {
             railGunTurret = new BlockRailGunTurret();
-            GameRegistry.register(railGunTurret);
-            GameRegistry.register(new ItemBlockRailGunTurret(railGunTurret));
         }
 
         leverBlock = new LeverBlock();
-        GameRegistry.register(leverBlock);
-        GameRegistry.register(new ItemBlockLever(leverBlock));
+        registerBlocks(ModBlocks.class);
     }
+
+
 
     public static void initTileEntities() {
         GameRegistry.registerTileEntity(TurretBase.class, Names.Blocks.turretBase);
