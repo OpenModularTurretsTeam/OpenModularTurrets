9e0a94ad7273fe0ed89586b8f1cfb61e0d7e0cb8
Keridos <fuffzehn@gmail.com>
2016-12-08 17:11:02 +0100

    fixing crashes when using configure screen in SMP

--- src/main/java/omtteam/openmodularturrets/client/gui/containers/ConfigContainer.java
+++ src/main/java/omtteam/openmodularturrets/client/gui/containers/ConfigContainer.java
@@ -1,7 +1,11 @@
 package omtteam.openmodularturrets.client.gui.containers;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IContainerListener;
+import omtteam.openmodularturrets.handler.NetworkingHandler;
+import omtteam.openmodularturrets.network.messages.MessageTurretBase;
 import omtteam.openmodularturrets.tileentity.TurretBase;
 
 public class ConfigContainer extends Container {
@@ -14,5 +18,15 @@ public class ConfigContainer extends Container {
     @Override
     public boolean canInteractWith(EntityPlayer player) {
         return tileEntity.isUseableByPlayer(player);
+    }
+
+    @Override
+    public void detectAndSendChanges() {
+        super.detectAndSendChanges();
+        for (IContainerListener listener : this.listeners) {
+            if (listener instanceof EntityPlayerMP) {
+                NetworkingHandler.INSTANCE.sendTo(new MessageTurretBase(this.tileEntity), (EntityPlayerMP) listener);
+            }
+        }
     }
 }
