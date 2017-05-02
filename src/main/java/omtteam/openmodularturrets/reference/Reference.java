f789dac48c4ff92ba4ddfe29363c219e6cf7c7ea
Keridos <fuffzehn@gmail.com>
2017-05-02 04:58:02 +0200

    some refactoring and min omlib required

--- src/main/java/omtteam/openmodularturrets/reference/Reference.java
+++ src/main/java/omtteam/openmodularturrets/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
     public static final String NAME = "Open Modular Turrets";
     public static final String VERSION = "@VERSION@";
     public static final String ACCEPTED_MINECRAFT_VERSION = "[1.9,1.12)";
-    private static final String OMLIB_MIN_VERSION = "3.0.0-65";
+    private static final String OMLIB_MIN_VERSION = "3.0.0-68";
     public static final String DEPENDENCIES = "after:ThermalFoundation;after:ThermalExpansion;after:OpenComputers;" +
             "after:ComputerCraft;after:Mekanism;after:EnderIO;after:Thaumcraft;required-after:omlib@" + OMLIB_MIN_VERSION;
 }
