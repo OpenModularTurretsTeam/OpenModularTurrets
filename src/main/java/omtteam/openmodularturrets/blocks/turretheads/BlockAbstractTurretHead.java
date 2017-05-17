2a9c3987851ad8007f8950d6abda0b2db4a3a83f
Keridos <fuffzehn@gmail.com>
2017-05-17 04:13:31 +0200

    and now turrets and lever have overlays, too

--- src/main/java/omtteam/openmodularturrets/blocks/turretheads/BlockAbstractTurretHead.java
+++ src/main/java/omtteam/openmodularturrets/blocks/turretheads/BlockAbstractTurretHead.java
@@ -7,20 +7,25 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import omtteam.omlib.blocks.BlockAbstractTileEntity;
 import omtteam.omlib.util.IHasItemBlock;
+import omtteam.omlib.util.MathUtil;
 import omtteam.openmodularturrets.OpenModularTurrets;
 import omtteam.openmodularturrets.tileentity.TurretBase;
+import omtteam.openmodularturrets.util.ITurretBaseAddon;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import static omtteam.openmodularturrets.util.TurretHeadUtil.getTurretBaseFacing;
+
 @SuppressWarnings("deprecation")
-public abstract class BlockAbstractTurretHead extends BlockAbstractTileEntity implements IHasItemBlock {
+public abstract class BlockAbstractTurretHead extends BlockAbstractTileEntity implements IHasItemBlock, ITurretBaseAddon {
     public static final PropertyBool CONCEALED = PropertyBool.create("concealed");
 
 
@@ -46,7 +51,18 @@ public abstract class BlockAbstractTurretHead extends BlockAbstractTileEntity im
         if (state.getValue(CONCEALED)) {
             return new AxisAlignedBB(0F, 0F, 0F, 0F, 0F, 0F);
         }
-        return new AxisAlignedBB(0.2F, 0.0F, 0.2F, 0.8F, 1F, 0.8F);
+        return new AxisAlignedBB(0.2F, 0.2F, 0.2F, 0.8F, 0.8F, 0.8F);
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBoxFromState(IBlockState blockState, World world, BlockPos pos) {
+        EnumFacing facing = getTurretBaseFacing(world, pos);
+        if (facing != null) {
+            AxisAlignedBB axisAlignedBB = new AxisAlignedBB(-0.3F, -0.3F, -0.5F, 0.3F, 0.3F, 0.5F);
+            axisAlignedBB = MathUtil.rotateAABB(axisAlignedBB, facing.getOpposite());
+            return axisAlignedBB.offset(pos).offset(0.5D, 0.5D, 0.5D);
+        }
+        return new AxisAlignedBB(0.2F, 0.0F, 0.2F, 0.8F, 1F, 0.8F).offset(pos);
     }
 
     @Override
