860d6e91e14961df91c5dfe6923c9e287c8bc8f3
Keridos <2742845+Keridos@users.noreply.github.com>
2017-05-21 03:09:31 +0200

    first implementation of the share owner command

--- src/main/java/omtteam/openmodularturrets/util/ITurretBaseAddonTileEntity.java
+++ src/main/java/omtteam/openmodularturrets/util/ITurretBaseAddonTileEntity.java
@@ -1,9 +1,12 @@
 package omtteam.openmodularturrets.util;
 
+import omtteam.omlib.util.IOwnedBlockAddon;
+
 /**
  * Created by Keridos on 17/05/17.
  * This Class
  */
-public interface ITurretBaseAddonTileEntity {
+public interface ITurretBaseAddonTileEntity extends IOwnedBlockAddon {
+
 
 }
