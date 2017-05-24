65351869402db08e9627c59579ae43441c40e14b
Keridos <fuffzehn@gmail.com>
2017-05-24 15:14:08 +0200

    fixing warning about projectile prefix on MC 1.11

--- src/main/java/omtteam/openmodularturrets/handler/ProjectileEntityHandler.java
+++ src/main/java/omtteam/openmodularturrets/handler/ProjectileEntityHandler.java
@@ -8,10 +8,10 @@ import omtteam.openmodularturrets.reference.Reference;
 
 public class ProjectileEntityHandler {
     public static void registerProjectiles(OpenModularTurrets mod) {
-        EntityTools.registerModEntity(new ResourceLocation(Reference.MOD_ID), RocketProjectile.class, Reference.MOD_ID + ":rocketProjectile", 1, mod, 64, 64, true);
-        EntityTools.registerModEntity(new ResourceLocation(Reference.MOD_ID), GrenadeProjectile.class, Reference.MOD_ID + ":grenadeProjectile", 2, mod, 64, 64, true);
-        EntityTools.registerModEntity(new ResourceLocation(Reference.MOD_ID), BulletProjectile.class, Reference.MOD_ID + ":bulletProjectile", 3, mod, 64, 64, true);
-        EntityTools.registerModEntity(new ResourceLocation(Reference.MOD_ID), LaserProjectile.class, Reference.MOD_ID + ":laserProjectile", 4, mod, 64, 64, true);
-        EntityTools.registerModEntity(new ResourceLocation(Reference.MOD_ID), BlazingClayProjectile.class, Reference.MOD_ID + ":blazingClayProjectile", 5, mod, 64, 64, true);
+        EntityTools.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":rocketProjectile"), RocketProjectile.class, Reference.MOD_ID + ":rocketProjectile", 1, mod, 64, 64, true);
+        EntityTools.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":grenadeProjectile"), GrenadeProjectile.class, Reference.MOD_ID + ":grenadeProjectile", 2, mod, 64, 64, true);
+        EntityTools.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":bulletProjectile"), BulletProjectile.class, Reference.MOD_ID + ":bulletProjectile", 3, mod, 64, 64, true);
+        EntityTools.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":laserProjectile"), LaserProjectile.class, Reference.MOD_ID + ":laserProjectile", 4, mod, 64, 64, true);
+        EntityTools.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":blazingClayProjectile"), BlazingClayProjectile.class, Reference.MOD_ID + ":blazingClayProjectile", 5, mod, 64, 64, true);
     }
 }
