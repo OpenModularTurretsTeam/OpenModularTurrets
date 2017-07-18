1c6cca5811fcaacf7ecf4521f78a3148d9f1f674
Keridos <2742845+Keridos@users.noreply.github.com>
2017-07-18 09:06:47 +0200

    fixing OC support in obfuscated environments, also some refactoring

--- src/main/java/omtteam/openmodularturrets/reference/Reference.java
+++ src/main/java/omtteam/openmodularturrets/reference/Reference.java
@@ -1,13 +1,10 @@
 package omtteam.openmodularturrets.reference;
 
-import static omtteam.omlib.compatability.ModCompatibility.*;
-
 public class Reference {
     public static final String MOD_ID = "openmodularturrets";
     public static final String NAME = "Open Modular Turrets";
     public static final String VERSION = "@VERSION@";
     public static final String ACCEPTED_MINECRAFT_VERSION = "[1.9,1.12)";
 
-    public static final String DEPENDENCIES = "after:" + TEModID + ";after:" + CCModID + ";after:" + MekModID +
-            ";after:" + EIOModID + ";after:" + TCModID + ";after:" + OCModID + ";required-after:omlib@[0.0.0,)";
+    public static final String DEPENDENCIES = "required-after:omlib@[0.0.0,)";
 }
