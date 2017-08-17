da5176bf5ea2797cf652dd45eaeb4acafed989e7
Keridos <2742845+Keridos@users.noreply.github.com>
2017-08-17 18:10:45 +0200

    a lot of refactoring, new API functions and fixed the mcmod.info

--- src/main/java/omtteam/openmodularturrets/api/IBaseTargetCheckController.java
+++ src/main/java/omtteam/openmodularturrets/api/IBaseTargetCheckController.java
@@ -2,14 +2,15 @@ package omtteam.openmodularturrets.api;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
+import omtteam.omlib.tileentity.EnumMachineMode;
 
 /**
  * Created by Keridos on 08/08/17.
  * This interface should be implemented by tile entities that want to control turret bases.
- * Register them with base.registerController(ITurretBaseController instance) for them to be used.
- * For turning bases off, use setMode() on the base.
+ * Register them with base.registerController(IBaseTargetCheckController instance) for them to be used.
+ * Turret Bases get additional API functions like getAllTargetsInRange. Only one of these can be registered per base.
  */
-public interface ITurretBaseController extends ITurretBaseAddonTileEntity {
+public interface IBaseTargetCheckController extends ITurretBaseAddonTileEntity {
     /**
      * Return true if the Entity given per parameter is a valid target.
      *
@@ -19,6 +20,20 @@ public interface ITurretBaseController extends ITurretBaseAddonTileEntity {
      * @return boolean
      */
     boolean isEntityValidTarget(Entity target, float yaw, float pitch);
+
+    /**
+     * Return true if the controller overrides Base modes. Should not be changed based on anything.
+     *
+     * @return boolean
+     */
+    boolean overridesMode();
+
+    /**
+     * Return the overriden mode if the controller overrides Base modes.
+     *
+     * @return overriding mode
+     */
+    EnumMachineMode getOverriddenMode();
 
     /**
      * Return the BlockPos of the tile entity implementing this.
