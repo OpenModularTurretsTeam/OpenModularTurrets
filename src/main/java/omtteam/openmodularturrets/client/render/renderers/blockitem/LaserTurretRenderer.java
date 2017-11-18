130ffc2221c47298664eb21a2de0dd1ba3ecbe34
Keridos <2742845+Keridos@users.noreply.github.com>
2017-11-18 14:27:22 +0100

    fixing desyncs when shift clicking, also new mappings

--- src/main/java/omtteam/openmodularturrets/client/render/renderers/blockitem/LaserTurretRenderer.java
+++ src/main/java/omtteam/openmodularturrets/client/render/renderers/blockitem/LaserTurretRenderer.java
@@ -29,7 +29,7 @@ class LaserTurretRenderer extends TileEntitySpecialRenderer {
 
     @SuppressWarnings("ConstantConditions")
     @Override
-    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
+    public void render(TileEntity te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
         TurretHead turretHead = (TurretHead) te;
         int rotation;
 
