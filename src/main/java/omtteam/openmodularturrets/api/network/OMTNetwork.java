de9d8e3deddc1ebd5dcf4ec44e5ab9b709a9e68c
Keridos <2742845+Keridos@users.noreply.github.com>
2018-02-04 05:31:21 +0100

    adding in saving and loading for networks, needs to be tested, also some commenting in network class

--- src/main/java/omtteam/openmodularturrets/api/network/OMTNetwork.java
+++ src/main/java/omtteam/openmodularturrets/api/network/OMTNetwork.java
@@ -4,9 +4,14 @@ import jline.internal.Nullable;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
 import omtteam.openmodularturrets.api.IBaseController;
 import omtteam.openmodularturrets.handler.OMTEventHandler;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static java.util.UUID.randomUUID;
@@ -21,12 +26,24 @@ public class OMTNetwork {
     private UUID uuid;
     private String name;
 
+    /**
+     * This is the default constructor for a network without a custom name.
+     * Names can be added to an existing network later.
+     *
+     * @param world the world the network is in
+     */
     public OMTNetwork(World world) {
         this.world = world;
         this.uuid = randomUUID();
         OMTEventHandler.getInstance().registerNetwork(this);
     }
 
+    /**
+     * This is the constructor for a network with a give name.
+     *
+     * @param world the world the network is in
+     * @param name  the name of the network
+     */
     public OMTNetwork(World world, String name) {
         this.world = world;
         this.uuid = randomUUID();
@@ -34,6 +51,27 @@ public class OMTNetwork {
         OMTEventHandler.getInstance().registerNetwork(this);
     }
 
+    /**
+     * This is the constructor that is called when the networks are being loaded from disk.
+     * Do NOT call this manually anywhere.
+     *
+     * @param world the world the network is in
+     * @param name  the name of the network
+     * @param uuid  the uuid of the network
+     */
+    public OMTNetwork(World world, String name, UUID uuid) {
+        this.world = world;
+        this.uuid = uuid;
+        this.name = name;
+        OMTEventHandler.getInstance().registerNetwork(this);
+        this.loadFromDisk(uuid, world);
+    }
+
+    /**
+     * This is the method that searches for energy giving and energy requiring devices and
+     * gathers and distributes the energy in the network
+     * Do NOT call this manually anywhere.
+     */
     public void tick() {
         List<IPowerExchangeTile> delivering = new ArrayList<>();
         List<IPowerExchangeTile> requiring = new ArrayList<>();
@@ -62,6 +100,9 @@ public class OMTNetwork {
         }
     }
 
+    /**
+     * This returns, if existing the TurretBaseController in the network.
+     */
     @Nullable
     public IBaseController getController() {
         for (INetworkTile device : devices.values()) {
@@ -72,6 +113,12 @@ public class OMTNetwork {
         return null;
     }
 
+    /**
+     * Use this to add a device to the network.
+     *
+     * @param tile The tile to be added
+     * @return true if possible, false if it failed.
+     */
     public boolean addDevice(INetworkTile tile) {
         boolean controllerExists = getController() != null;
         if (tile instanceof IBaseController && !controllerExists) {
@@ -84,18 +131,37 @@ public class OMTNetwork {
         return false;
     }
 
+    /**
+     * Use this to remove a device from the network.
+     *
+     * @param tile The tile to be added
+     * @return true if possible, false if it failed.
+     */
     public boolean removeDevice(INetworkTile tile) {
         return this.devices.remove(tile.getPosition()) != null;
     }
 
+    /**
+     * Use this to get a device on a certain position from the network.
+     *
+     * @param pos The BlockPos of the required device
+     * @return if loaded and existing, the device, null otherwise
+     */
     @Nullable
     public INetworkTile getConnectedDevice(BlockPos pos) {
         return world.isBlockLoaded(pos) ? devices.get(pos) : null;
     }
 
+    /**
+     * This gives back a Collection of all the connected devices.
+     *
+     * @return the collection
+     */
     public Collection<INetworkTile> getAllDevices() {
         return devices.values();
     }
+
+    // Getters and Setters
 
     public World getWorld() {
         return world;
@@ -108,6 +174,8 @@ public class OMTNetwork {
     public String getName() {
         return name;
     }
+
+    //Utility Functions
 
     public void splitNetwork() {
         // set all networktile to refresh their network.
@@ -142,5 +210,50 @@ public class OMTNetwork {
         this.world = null;
     }
 
-    //TODO: add saving and loading function
+    public void saveToDisk() {
+        File saveRoot = DimensionManager.getCurrentSaveRootDirectory();
+        if (saveRoot != null) {
+            Path path = Paths.get(saveRoot.toString() + "/omt/");
+            Path fullpath = Paths.get(saveRoot.toString() + "/omt/network-" + this.getUuid().toString() + ".sav");
+            try {
+                if (Files.notExists(path)) {
+                    if (!path.toFile().mkdir()) {
+                        throw new Exception("Failed to create dir");
+                    }
+                }
+                FileOutputStream saveFile = new FileOutputStream(fullpath.toFile());
+                ObjectOutputStream save = new ObjectOutputStream(saveFile);
+                save.writeObject(this.devices.keySet());
+                save.close();
+                saveFile.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                try {
+                    Files.deleteIfExists(fullpath);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void loadFromDisk(UUID uuid, World world) {
+        try {
+            Set<BlockPos> tempList = new HashSet<>();
+            Path fullpath = Paths.get(DimensionManager.getCurrentSaveRootDirectory().toString() + "/omt/network- " + uuid.toString() + ").sav");
+            FileInputStream saveFile = new FileInputStream(fullpath.toFile());
+            ObjectInputStream save = new ObjectInputStream(saveFile);
+            Object object = save.readObject();
+            if (object instanceof Set) {
+                tempList = (HashSet<BlockPos>) object;
+            }
+            save.close();
+            saveFile.close();
+            for (BlockPos pos : tempList) {
+                devices.put(pos, (INetworkTile) world.getTileEntity(pos));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
