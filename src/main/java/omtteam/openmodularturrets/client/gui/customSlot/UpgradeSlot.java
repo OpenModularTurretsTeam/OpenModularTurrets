4c3cebb4f6ca45d129adf80eb2f9e1168f195206
Keridos <fuffzehn@gmail.com>
2018-05-08 23:11:48 +0200

    updating inventory to support 1.12+ capabilities.

--- src/main/java/omtteam/openmodularturrets/client/gui/customSlot/UpgradeSlot.java
+++ src/main/java/omtteam/openmodularturrets/client/gui/customSlot/UpgradeSlot.java
@@ -3,13 +3,15 @@ package omtteam.openmodularturrets.client.gui.customSlot;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
 import omtteam.openmodularturrets.items.UpgradeMetaItem;
 
 
-public class UpgradeSlot extends Slot {
+public class UpgradeSlot extends SlotItemHandler {
     @SuppressWarnings("SameParameterValue")
-    public UpgradeSlot(IInventory par1iInventory, int par2, int par3, int par4) {
-        super(par1iInventory, par2, par3, par4);
+    public UpgradeSlot(IItemHandler inventory, int par2, int par3, int par4) {
+        super(inventory, par2, par3, par4);
     }
 
     @Override
