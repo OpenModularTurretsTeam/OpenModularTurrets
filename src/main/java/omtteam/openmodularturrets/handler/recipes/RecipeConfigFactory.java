2650b76fe131a0df915aff2d0acea74391e676f1
Keridos <2742845+Keridos@users.noreply.github.com>
2018-05-08 19:02:17 +0200

    reworked recipes

---
+++ src/main/java/omtteam/openmodularturrets/handler/recipes/RecipeConfigFactory.java
@@ -0,0 +1,43 @@
+package omtteam.openmodularturrets.handler.recipes;
+
+import com.google.gson.JsonObject;
+import net.minecraft.util.JsonUtils;
+import net.minecraftforge.common.crafting.IConditionFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+import omtteam.openmodularturrets.compatibility.ModCompatibility;
+import omtteam.openmodularturrets.handler.OMTConfigHandler;
+
+import java.util.function.BooleanSupplier;
+
+import static omtteam.omlib.compatibility.ModCompatibility.ComputerCraftLoaded;
+import static omtteam.omlib.compatibility.ModCompatibility.OpenComputersLoaded;
+
+/**
+ * Created by Keridos on 13/11/17.
+ * This Class
+ */
+public class RecipeConfigFactory implements IConditionFactory {
+    @Override
+    public BooleanSupplier parse(JsonContext context, JsonObject json) {
+        String mod = JsonUtils.getString(json, "mod");
+        if (ModCompatibility.EnderIOLoaded && OMTConfigHandler.recipes.equals("enderio") && mod.equals("enderio")) {
+            return () -> true;
+        } else if (ModCompatibility.MekanismLoaded && OMTConfigHandler.recipes.equals("mekanism") && mod.equals("mekanism")) {
+            return () -> true;
+        } else if (OMTConfigHandler.recipes.equals("vanilla") && mod.equals("vanilla")) {
+            return () -> true;
+        } else if (OMTConfigHandler.recipes.equals("auto")) {
+            if (ModCompatibility.EnderIOLoaded && mod.equals("enderio")) {
+                return () -> true;
+            } else if (ModCompatibility.MekanismLoaded && !ModCompatibility.EnderIOLoaded && mod.equals("mekanism")) {
+                return () -> true;
+            } else if (mod.equals("vanilla") && !ModCompatibility.MekanismLoaded && !ModCompatibility.EnderIOLoaded) {
+                return () -> true;
+            }
+        }
+        if (mod.equals("computer") && (ComputerCraftLoaded || OpenComputersLoaded)) {
+            return () -> true;
+        }
+        return () -> false;
+    }
+}
--- src/main/java/omtteam/openmodularturrets/handler/recipes/ThaumcraftRecipeHandler.java
+++
@@ -1,9 +0,0 @@
-package omtteam.openmodularturrets.handler.recipes;
-
-
-@SuppressWarnings("unused")
-class ThaumcraftRecipeHandler {
-    /*public static void init() {
-        GJSONRecipeBuilder.addShapedRecipe(ModItems.addonPotentia, " A ", "ABA", " A ", 'A', "ingotThaumium", 'B',ModItems.redstoneReactorAddon);
-    } */
-}
