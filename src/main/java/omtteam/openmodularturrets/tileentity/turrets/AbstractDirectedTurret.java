83c30c29d07b712caec0b80822319a1a28468216
Keridos <2742845+Keridos@users.noreply.github.com>
2020-01-30 17:39:23 +0100

    CRelease: fixing turret rotation on VS Ships

--- src/main/java/omtteam/openmodularturrets/tileentity/turrets/AbstractDirectedTurret.java
+++ src/main/java/omtteam/openmodularturrets/tileentity/turrets/AbstractDirectedTurret.java
@@ -7,14 +7,20 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import omtteam.omlib.util.NetworkUtil;
 import omtteam.openmodularturrets.blocks.turretheads.BlockAbstractTurretHead;
+import omtteam.openmodularturrets.compatibility.ModCompatibility;
 import omtteam.openmodularturrets.handler.OMTNetworkingHandler;
 import omtteam.openmodularturrets.handler.config.OMTConfig;
 import omtteam.openmodularturrets.network.messages.MessageUpdateTurret;
 import omtteam.openmodularturrets.util.TurretHeadUtil;
+import valkyrienwarfare.api.IPhysicsEntity;
+import valkyrienwarfare.api.IPhysicsEntityManager;
+import valkyrienwarfare.api.TransformType;
 
 import javax.annotation.Nullable;
 
@@ -175,8 +181,20 @@ public abstract class AbstractDirectedTurret extends TurretHead {
         }
 
         if (this.ticks % 5 == 0) {
-            OMTNetworkingHandler.INSTANCE.sendToAllAround(new MessageUpdateTurret(this.pos, this.yaw, this.pitch),
-                                                          NetworkUtil.getTargetPointFromTE(this, 100));
+            if (ModCompatibility.ValkyrienWarfareLoaded) {
+                IPhysicsEntity physicsEntity = IPhysicsEntityManager.INSTANCE.getPhysicsEntityFromShipSpace(getWorld(),
+                                                                                                            getPos());
+                BlockPos pos = this.getPos();
+                if (physicsEntity != null) {
+                    pos = new BlockPos(physicsEntity.transformVector(new Vec3d(pos), TransformType.SUBSPACE_TO_GLOBAL));
+                }
+                OMTNetworkingHandler.INSTANCE.sendToAllAround(
+                        new MessageUpdateTurret(this.pos, this.yaw, this.pitch),
+                        NetworkUtil.getTargetPointFromBlockPos(this.getWorld().provider.getDimension(), pos, 100));
+            } else {
+                OMTNetworkingHandler.INSTANCE.sendToAllAround(new MessageUpdateTurret(this.pos, this.yaw, this.pitch),
+                                                              NetworkUtil.getTargetPointFromTE(this, 100));
+            }
         }
         this.ticks++;
 
