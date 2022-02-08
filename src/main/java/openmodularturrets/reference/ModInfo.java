952e4a30689f23082cabf461a18a2b69f6fdf121
bombcar <github@bombcar.com>
2022-02-08 17:05:54 -0600

    update buildscript

--- src/main/java/openmodularturrets/reference/ModInfo.java
+++ src/main/java/openmodularturrets/reference/ModInfo.java
@@ -1,8 +1,8 @@
 package openmodularturrets.reference;
 
 public class ModInfo {
-    public static final String ID = "openmodularturrets";
-    public static final String NAME = "Open Modular Turrets";
-    public static final String VERSION = "@VERSION@";
+    public static final String ID = "GRADLETOKEN_MODID";
+    public static final String NAME = "GRADLETOKEN_MODNAME";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String DEPENDENCIES = "after:ThermalFoundation;after:ThermalExpansion;after:OpenComputers;after:ComputerCraft;after:Mekanism;after:EnderIO;after:Thaumcraft";
 }
